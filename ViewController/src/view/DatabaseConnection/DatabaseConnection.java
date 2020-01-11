e11d5de97fd03eff232c5d996702c42495658ccf
ADFDBSERVER <ADFDBSERVER@DESKTOP-M6O26BQ>
2020-01-11 18:05:32 +0500

    mon insp end time done and db to live

--- ViewController/src/view/DatabaseConnection/DatabaseConnection.java
+++ ViewController/src/view/DatabaseConnection/DatabaseConnection.java
@@ -24,7 +24,7 @@ public class DatabaseConnection {
         /* Database credentials >> username , password , hostname , port , SID  */
         setUNamee("emfp");
         setUpasss("emfp");
-        setUipp("192.168.1.2");
+        setUipp("207.180.246.67");
         setUportADF("1521");
         setUSID("orcl");
         /* END  */
